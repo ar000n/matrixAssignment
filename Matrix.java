08769f9b15841e31ae5355f050302cba6a430081
Arun Sathian <arunsathian@gmail.com>
2016-02-22 18:29:48 +0530

    matrix multiplication implimented

--- Matrix.java
+++ Matrix.java
@@ -1,7 +1,7 @@
 public class Matrix {
-  int row;
-  int column;
-  int [][] matrix;
+  private int row;
+  private int column;
+  private int [][] matrix;
 
   private Matrix(int row,int column){
   	this.row = row;
@@ -19,6 +19,48 @@ public class Matrix {
   	}
   	Matrix res = Matrix.createMatrix(row,column,sum);
   	return res;
+  }
+
+  public int[] convertTwoDimensionToUni(int[][] matrix){
+  	int row = matrix.length;
+  	int column = matrix[0].length;
+  	int []result = new int[row * column];
+  	int count = 0;
+  	for (int i=0;i<row ;i++ ) {
+  		for (int j=0;j<column ;j++,count++ ) {
+			result[count] = matrix[i][j];		
+  		}
+  	}
+  	return result;
+  }
+  public Matrix multiplyWith(Matrix matrix2){
+  	if(this.column == matrix2.row){
+	  	int[][] product = new int[this.row][(matrix2.column)];
+	  	for (int i = 0; i < this.row; i++) {
+           for (int j = 0; j < matrix2.column; j++) {
+               for (int k = 0; k < this.column; k++) {
+                   product[i][j] = product[i][j] + this.matrix[i][k] * matrix2.matrix[k][j];
+               }
+           }
+	    }
+	  	int[] result =convertTwoDimensionToUni(product);
+	  	Matrix res = Matrix.createMatrix(row,column,result);
+	  	return res;
+	}
+	else{
+		return null;
+	}
+  }
+
+  public boolean isEqualTo(Matrix result){
+    for (int i=0;i<this.row ;i++ ) {
+      for(int j=0;j<this.column;j++){
+        if(this.matrix[i][j] != result.matrix[i][j]){
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   public static Matrix createMatrix(int row,int column,int [] elements){
--- MatrixTest.java
+++ MatrixTest.java
@@ -1,19 +1,40 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class MatrixTest {
   @Test
-  public void createAMatrix() {
+  public void matrixAddition() {
   	int [] ele = {1,2,3,4,5,6,7,8,9};
-  	int[][]sum = {{2,4,6},{8,10,12},{14,16,18}};
+  	int[]elements = {2,4,6,8,10,12,14,16,18};
+    Matrix sum = Matrix.createMatrix(3,3,elements);
     Matrix matrix1 = Matrix.createMatrix(3,3,ele);
     Matrix matrix2 = Matrix.createMatrix(3,3,ele);
     Matrix result = matrix1.addMatrix(matrix2);
-    for(int i=0;i<result.row;i++){
-    	for(int j=0;j<result.column;j++){
-    		assertEquals(sum[i][j],result.matrix[i][j]);
-    		
-    	}
-    }
+  }
+
+  @Test
+  public void matrixMultiplication(){
+  	int [] ele1 = {2,3,4,5};
+  	int [] ele2 = {3,1,2,6};
+    int [] elements = {12,20,22,34};
+  	Matrix matrix1 = Matrix.createMatrix(2,2,ele1);
+    Matrix matrix2 = Matrix.createMatrix(2,2,ele2);
+    Matrix result = matrix1.multiplyWith(matrix2);
+    Matrix product = Matrix.createMatrix(2,2,elements);
+    assertTrue(product.isEqualTo(result));
+  }
+
+  @Test
+  public void matrixMultiplication_if_couldnot_do(){
+    int [] ele1 = {2,3,4,5};
+    int [] ele2 = {3,1,2,6,3,4};
+    // int [] elements = {12,20,22,34};
+    Matrix matrix1 = Matrix.createMatrix(2,2,ele1);
+    Matrix matrix2 = Matrix.createMatrix(3,2,ele2);
+    Matrix result = matrix1.multiplyWith(matrix2);
+    // Matrix product = Matrix.createMatrix(2,2,elements);
+    assertEquals(null,result);
   }
 }
